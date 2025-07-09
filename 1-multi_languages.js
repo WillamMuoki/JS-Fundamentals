ea65a8f472b96d82f60c10abe516ba641c123772
William muoki <kyalowilliam969@gmail.com>
2025-07-09 22:41:55 +0300

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,4 +1,5 @@
 #!usr/bin/node
-console.log("c isfun");
+console.log("c is fun");
 console.log("python is cool");
 console.log("JavaScript is amazing");
+console.log("c isfun" , "pythone is cool", JavaScript is amazing"); 
