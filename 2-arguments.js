ac45955431ac926b933c644c2597f521f2ccf631
William muoki <kyalowilliam969@gmail.com>
2025-07-09 23:38:36 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,1 +1,7 @@
-
+const arguments passed = 0
+if (arguments passed.length > 1) {
+console.log('Arguments found'); 
+} else if (arguments passed.length = 1) {
+console.log ('Argument found'); }
+else { console.log ('No argument');
+     }    
