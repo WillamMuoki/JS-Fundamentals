17376eda2fce51fd92b815ccda668313f541a1d3
William muoki <kyalowilliam969@gmail.com>
2025-07-09 23:14:21 +0300

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,9 +1,8 @@
 #!usr/bin/node
-const lines = [
-'c is fun',
-'python is cool',
-'JavaScript is amazing'
- ];
-for (const line of lines) {
- console.log(line);
-}
+ const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
