5dca935a6923a885fa9ad00be7d2fc737d69488e
William muoki <kyalowilliam969@gmail.com>
2025-07-09 23:00:44 +0300

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,8 +1,8 @@
 #!usr/bin/node
 const lines = [
-console.log"c is fun";
-console.log"python is cool";
-console.log"JavaScript is amazing";
+'c is fun';
+'python is cool';
+'JavaScript is amazing';
  ];
 for (let i = 0; i < lines.length;i++) {
   console.log(lines[i]);
